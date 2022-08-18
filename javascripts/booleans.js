f2031b3776284c6e269aa95a8e83f75f32d306a8
gzod1016 <60858393+gzod1016@users.noreply.github.com>
2022-08-18 15:28:34 -0400

    Part 3 Booleans

---
+++ javascripts/booleans.js
@@ -0,0 +1,16 @@
+
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = 'Kevin'
+
+console.log(a<b);
+console.log(c>d);
+console.log('Name'=='Name');
+console.log(true || false);
+console.log(false &&false &&false &&false &&false || true)
+console.log(e === 'Kevin')
+console.log(a+b==c);
+console.log(a*a==d);
+console.log(48=='48')
