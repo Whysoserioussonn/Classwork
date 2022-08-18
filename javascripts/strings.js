3bc4678eceb1bb28f8bd2caf318ac7eab9c0b621
gzod1016 <60858393+gzod1016@users.noreply.github.com>
2022-08-18 15:11:07 -0400

    part B strings

---
+++ javascripts/strings.js
@@ -0,0 +1,15 @@
+
+//B.Strings
+//1) create a variable called firstVarible\
+
+
+let firstVar = "Hello World!";
+firstVar = 5;
+let secondVar = firstVar;
+secondVar= 'Morning!';
+//value is 5
+let yourName = "Henry";
+let newStr = 'Hello my name is ' + yourName;
+
+console.log(firstVar)
+console.log(newStr)
